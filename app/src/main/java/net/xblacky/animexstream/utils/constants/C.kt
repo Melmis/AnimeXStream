c863f6c10ac5069222fd0d574920ae21de652c63
Melmis <64072341+Melmis@users.noreply.github.com>
2020-10-18 18:54:46 +0300

    Update C.kt
    The URL on gogoanime seems to have changed so could you update the URL for the code of the app?

--- app/src/main/java/net/xblacky/animexstream/utils/constants/C.kt
+++ app/src/main/java/net/xblacky/animexstream/utils/constants/C.kt
@@ -53,8 +53,8 @@ class C {
 
         //Network Requests Header
         const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
-        const val ORIGIN = "https://www1.gogoanime.movie"
-        const val  REFERER = "https://www1.gogoanime.movie/"
+        const val ORIGIN = "https://gogoanime.so/"
+        const val  REFERER = "https://gogoanime.so/"
 
         //Realm
         const val MAX_TIME_M3U8_URL = 2 * 60 * 60 *1000
